112145d60b8bf7a23fd8f176f2a655cdec551d1a
j1anb1n <chan.jianbin@gmail.com>
2012-07-06 14:27:49 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-var ApiServer = new require('apiserver');
+var ApiServer = require('apiserver');
+var settings = require('./settings.json');
+var mongoose = require('mongoose');
+
+// Modules
 var UserModule = require('./module/User');
 
-
+// init
 var API = new ApiServer({port: 8888});
+
+// config
 API.use(ApiServer.payloadParser());
-
-
 API.addModule('v1', 'UserModule', UserModule);
-
 
 API.router.addRoutes([
     ['/user/:username/?', 'v1/UserModule#user']
 ]);
 
+// run
 API.listen();
